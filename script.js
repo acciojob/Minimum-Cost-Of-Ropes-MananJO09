e288728d32060c333a16c6b8a91c97ceb8bf2321
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-02 12:07:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
-button.onclick = function () {
-  const inputField = document.querySelector('input[type="text"]');
-  const result = document.getElementById('result');
-  
-  // Get the input values and convert them into an array of integers
-  const arr = inputField.value.split(',').map(Number);
-  const n = arr.length;
-  
-  const minCost = calculateMinCost(arr, n);
-  // Update the result div with the calculated minimum cost
-  result.textContent = minCost;
-}
+function calculateMinCost() {
 
-function calculateMinCost(arr,n) {
+  var inputElement = document.getElementById('rope-lengths').value;
+  var arr = inputElement.split(',');
+   arr.sort(function (a,b) {return a-b});
+
+ var cost = 0;
+ while (arr.length>1){
+  var res = Number(arr[0])+ Number(arr[1]);
+        arr.shift()
+        arr.shift()
+  arr.push(res);
+  cost +=res;
+
+  
+
+  arr.sort(function (a,b) {return a-b})
+ }
   //your code here
-	let pq=[];
-	for (let i = 0; i < n; i++) {
-		pq.push(arr[i]);
-	}
-	pq.sort(function (a,b) {
-		return a-b;
-	});
-	let res=0;
-  while (pq.length > 1) {
-		// Extract shortest two ropes from pq
-		let first = pq.shift();
-		let second = pq.shift();
-
-		// Connect the ropes: update result
-		// and insert the new rope to pq
-		res += first + second;
-		pq.push(first + second);
-		pq.sort(function(a,b){return a-b;});
-	}
-   
-}  
-document.getElementById("result").innerHTML = res;
+  document.getElementById("result").innerHTML = cost
+  
+}
