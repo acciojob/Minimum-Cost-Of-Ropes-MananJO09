a4e81f2079037a7fbf90e35d36d8245d35c27754
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-02 12:04:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,89 +1,37 @@
 button.onclick = function () {
-const inputField = document.querySelector('input[type="text"]');
-const result = document.getElementById('result');
-
-// Get the input values and convert them into an array of integers
-const arr = inputField.value.split(',').map(Number);
-const n = arr.length;
-
-const minCost = calculateMinCost(arr, n);
-// Update the result div with the calculated minimum cost
-result.textContent = minCost;
+  const inputField = document.querySelector('input[type="text"]');
+  const result = document.getElementById('result');
+  
+  // Get the input values and convert them into an array of integers
+  const arr = inputField.value.split(',').map(Number);
+  const n = arr.length;
+  
+  const minCost = calculateMinCost(arr, n);
+  // Update the result div with the calculated minimum cost
+  result.textContent = minCost;
 }
 
-function calculateMinCost(arr, n) {
-	let minHeap = new PriorityQueue((a, b) => a - b);
+function calculateMinCost(arr,n) {
+  //your code here
+	let pq=[];
 	for (let i = 0; i < n; i++) {
-	minHeap.push(arr[i]);
+		pq.push(arr[i]);
 	}
+	pq.sort(function (a,b) {
+		return a-b;
+	});
+	let res=0;
+  while (pq.length > 1) {
+		// Extract shortest two ropes from pq
+		let first = pq.shift();
+		let second = pq.shift();
 
-	let res = 0;
-	while (minHeap.size() > 1) {
-	let first = minHeap.pop();
-	let second = minHeap.pop();
-	
-	res += first + second;
-	minHeap.push(first + second);
+		// Connect the ropes: update result
+		// and insert the new rope to pq
+		res += first + second;
+		pq.push(first + second);
+		pq.sort(function(a,b){return a-b;});
 	}
-	return res;
-}
-
-class PriorityQueue {
-	constructor(comparator) {
-	this._heap = [];
-	this._comparator = comparator;
-	}
-	
-	size() {
-	return this._heap.length;
-	}
-	
-	isEmpty() {
-	return this.size() === 0;
-}
-
-peek() {
-	return this._heap[0];
-}
-
-push(value) {
-	this._heap.push(value);
-	this._siftUp();
-	}
-
-pop() {
-	const poppedValue = this.peek();
-	const bottom = this.size() - 1;
-	if (bottom > 0) {
-	this._swap(0, bottom);
-	}
-	this._heap.pop();
-	this._siftDown();
-	return poppedValue;
-	}
-	
-	_parent(i) {
-	return ((i + 1) >>> 1) - 1;
-	}
-	
-	_left(i) {
-	return (i << 1) + 1;
-	}
-	
-	_right(i) {
-	return (i + 1) << 1;
-	}
-	
-	_greater(i, j) {
-	return this._comparator(this._heap[i], this._heap[j]) < 0;
-	}
-	
-	_swap(i, j) {
-	[this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
-	}
-	
-	_siftUp() {
-	let node = this.size() - 1;
-	while (node > 0 && this._greater(node, this._parent(node))) {
-	this._swap(node, this._parent(node));
-	node = this._parent(node);
+   
+}  
+document.getElementById("result").innerHTML = res;
