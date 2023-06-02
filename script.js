c767ca8c9df2b10ce8a985c1526ec918a39eafaf
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-02 10:58:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function calculateMinCost() {
+function calculateMinCost(arr,n) {
   //your code here
-  
-  
-  
+	let pq=[];
+	for (let i = 0; i < n; i++) {
+		pq.push(arr[i]);
+	}
+	pq.sort(function (a,b) {
+		return a-b;
+	});
+	let res=0;
+  while (pq.length > 1) {
+		// Extract shortest two ropes from pq
+		let first = pq.shift();
+		let second = pq.shift();
+
+		// Connect the ropes: update result
+		// and insert the new rope to pq
+		res += first + second;
+		pq.push(first + second);
+		pq.sort(function(a,b){return a-b;});
+	}
+    return res;
 }  
